cdca084e15cff3b101429c1506c153c5f41d0d4c
shlocko <mcdude22@gmail.com>
2021-11-28 18:12:58 -0800

    Began laying foundation of ECS and fixed entity component map

--- src/com/company/Entity.java
+++ src/com/company/Entity.java
@@ -1,9 +1,10 @@
 package com.company;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Entity {
-    private Map<String, Component> components;
+    private Map<String, Component> components = new HashMap<>();
 
     public void addComponent(Component component){
         components.put(component.getComponentName(), component);
